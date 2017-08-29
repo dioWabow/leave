117696dd97f79e84546b52571d88ce0e3957146b
eno <eno@wabow.com>
2017-08-29 22:07:03 +0800

    config頁面更新、db取值方式修改為coding style

--- app/Config.php
+++ app/Config.php
@@ -7,18 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class Config extends Model
 {
     //可以傳入數值的欄位
-    protected $fillable = ['config_key', 'config_value'];
-    
-    /**
-     * 與Model關聯的table
-     *
-     * @var string
-     */
-    protected $table = 'configs';
+    protected $fillable = [
+        'config_key',
+        'config_value',
+        'comment',
+    ];
 
-    public function getValue($key){
-        $result = $this->where('config_key','=', $key)->pluck('config_value');
-		return $result;
+    /**
+     * 搜尋table單個資料
+     *
+     * @param  array   $where     搜尋條件
+     * @return 資料object/false
+     */
+    public static function getConfigByKey($key = "")
+    {
+
+        $query = self::where("config_key", $key);
+
+        $result = $query->pull('config_value');
+        dd($result);
+        return $result;
     }
 
 }
--- resources/views/system_conf.blade.php
+++ resources/views/system_conf.blade.php
@@ -27,50 +27,50 @@
 						<div class="form-group">
 							<label for="config_company_name" class="col-sm-2 control-label">公司名稱</label>
 							<div class="col-sm-5">
-								<input type="text" class="form-control" id="config_company_name" name="config[company_name]" placeholder="Company Name" value="{{ $config->getValue('company_name')[0] }}">
+								<input type="text" class="form-control" id="config_company_name" name="config[company_name]" placeholder="Company Name" value="{{ $config->getConfigByKey('company_name') }}">
 							</div>
 
 							<label for="config_company_sort_name" class="col-sm-2 control-label">簡稱</label>
 							<div class="col-sm-3">
-								<input type="text" class="form-control" id="config_company_sort_name" name="config[company_sort_name]" placeholder="Company sort name" value="{{ $config->getValue('company_sort_name')[0] }}">
+								<input type="text" class="form-control" id="config_company_sort_name" name="config[company_sort_name]" placeholder="Company sort name" value="{{ $config->getConfigByKey('company_sort_name') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_company_website" class="col-sm-2 control-label">網址</label>
 							<div class="col-sm-10">
-								<input type="text" class="form-control" id="config_company_website" name="config[company_website]" placeholder="Website" value="{{ $config->getValue('company_website')[0] }}">
+								<input type="text" class="form-control" id="config_company_website" name="config[company_website]" placeholder="Website" value="{{ $config->getConfigByKey('company_website') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_company_logo" class="col-sm-2 control-label">LOGO</label>
 							<div class="col-sm-10">
-								<input id="config_company_logo" name="config[company_logo]" class="file" type="file" value="{{ $config->getValue('company_logo')[0] }}">
+								<input id="config_company_logo" name="config[company_logo]" class="file" type="file" value="{{ $config->getConfigByKey('company_logo') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_company_logo" class="col-sm-2 control-label"></label>
-                            @if( $config->getValue('company_logo')[0] != "") 
+                            @if( $config->getConfigByKey('company_logo') != "") 
 							<div class="col-sm-10">
-								<img src = ".\dist\img\{{ $config->getValue('company_logo')[0] }}">
+								<img src = ".\dist\img\{{ $config->getConfigByKey('company_logo') }}">
 							</div>
                             @endif
 						</div>
 						<div class="form-group">
 							<label for="config_company_rules" class="col-sm-2 control-label">人事規章</label>
 							<div class="col-sm-10">
-								<input type="text" class="form-control" id="config_company_rules" name="config[company_rules]" placeholder="URL" value="{{ $config->getValue('company_rules')[0] }}">
+								<input type="text" class="form-control" id="config_company_rules" name="config[company_rules]" placeholder="URL" value="{{ $config->getConfigByKey('company_rules') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_company_domamin" class="col-sm-2 control-label">異常回報</label>
 							<div class="col-sm-10">
-								<input type="text" class="form-control" id="config_company_mail" name="config[company_mail]" placeholder="E-MAIL" value="{{ $config->getValue('company_mail')[0] }}">
+								<input type="text" class="form-control" id="config_company_mail" name="config[company_mail]" placeholder="E-MAIL" value="{{ $config->getConfigByKey('company_mail') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_company_mail" class="col-sm-2 control-label">網域</label>
 							<div class="col-sm-10">
-								<input type="text" class="form-control" id="config_company_domain" name="config[company_domain]" placeholder="Wabow.com" value="{{ $config->getValue('company_domain')[0] }}">
+								<input type="text" class="form-control" id="config_company_domain" name="config[company_domain]" placeholder="Wabow.com" value="{{ $config->getConfigByKey('company_domain') }}">
 							</div>
 						</div>
 					</div>
@@ -94,25 +94,25 @@
 					<label for="config_smtp_host" class="col-sm-2 control-label">HOST</label>
 
 					<div class="col-sm-7">
-						<input type="email" class="form-control" id="config_smtp_host" name="config[config_smtp_host]" placeholder="ex: smtp.gmail.com" value="{{ $config->getValue('config_smtp_host')[0] }}">
+						<input type="email" class="form-control" id="config_smtp_host" name="config[config_smtp_host]" placeholder="ex: smtp.gmail.com" value="{{ $config->getConfigByKey('config_smtp_host') }}">
 					</div>
 
 					<label for="config_smtp_port" class="col-sm-1 control-label">Port</label>
 					<div class="col-sm-2">
-						<input type="text" class="form-control" id="config_smtp_port" name="config[smtp_port]" placeholder="465" value="{{ $config->getValue('smtp_port')[0] }}">
+						<input type="text" class="form-control" id="config_smtp_port" name="config[smtp_port]" placeholder="465" value="{{ $config->getConfigByKey('smtp_port') }}">
 					</div>
 					</div>
 					<div class="form-group">
 					<label for="config_smtp_username" class="col-sm-2 control-label">Username</label>
 
 					<div class="col-sm-4">
-						<input type="text" class="form-control" id="config_smtp_username" name="config[smtp_username]" placeholder="ex: test-leave@gmail.com" value="{{ $config->getValue('smtp_username')[0] }}">
+						<input type="text" class="form-control" id="config_smtp_username" name="config[smtp_username]" placeholder="ex: test-leave@gmail.com" value="{{ $config->getConfigByKey('smtp_username') }}">
 					</div>
 
 					<label for="config_smtp_password" class="col-sm-2 control-label">Password</label>
 
 					<div class="col-sm-4">
-						<input type="password" class="form-control" id="config_smtp_password" name="config[smtp_password]" placeholder="請輸入密碼" value="{{ $config->getValue('smtp_password')[0] }}">
+						<input type="password" class="form-control" id="config_smtp_password" name="config[smtp_password]" placeholder="請輸入密碼" value="{{ $config->getConfigByKey('smtp_password') }}">
 					</div>
 					</div>
 
@@ -120,11 +120,11 @@
 						<label for="config_smtp_auth" class="col-sm-2 control-label">Auth</label>
 						<div class="col-sm-10">
 							<label>
-								<input type="radio" name="config[smtp_auth]" class="flat-red" value="true" @if ( $config->getValue('smtp_auth')[0]  == 'true') checked="checked" @endif >
+								<input type="radio" name="config[smtp_auth]" class="flat-red" value="true" @if ( $config->getConfigByKey('smtp_auth')  == 'true') checked="checked" @endif >
 								是
 							</label>&emsp;
 							<label>
-								<input type="radio" name="config[smtp_auth]" class="flat-red" value="false" @if ( $config->getValue('smtp_auth')[0]  == 'false') checked="checked" @endif>
+								<input type="radio" name="config[smtp_auth]" class="flat-red" value="false" @if ( $config->getConfigByKey('smtp_auth')  == 'false') checked="checked" @endif>
 								否
 							</label>&emsp;
 						</div>
@@ -134,13 +134,13 @@
 					<label for="config_smtp_from" class="col-sm-2 control-label">From</label>
 
 					<div class="col-sm-4">
-						<input type="text" class="form-control" id="config_smtp_from" name="config[smtp_from]" placeholder="hello" value="{{ $config->getValue('smtp_from')[0] }}">
+						<input type="text" class="form-control" id="config_smtp_from" name="config[smtp_from]" placeholder="hello" value="{{ $config->getConfigByKey('smtp_from') }}">
 					</div>
 
 					<label for="config_smtp_display" class="col-sm-2 control-label">Display</label>
 
 					<div class="col-sm-4">
-						<input type="text" class="form-control" id="config_smtp_display" name="config[smtp_display]" placeholder="請假機器" value="{{ $config->getValue('smtp_display')[0] }}">
+						<input type="text" class="form-control" id="config_smtp_display" name="config[smtp_display]" placeholder="請假機器" value="{{ $config->getConfigByKey('smtp_display') }}">
 					</div>
 					</div>
 				</div>
@@ -167,11 +167,11 @@
 							<label for="form_config_google_login" class="col-sm-3 control-label">Google-自動登入</label>
 							<div class="col-sm-9">
 								<label>
-									<input type="radio" name="config[google_status]" class="flat-red" value="true" @if ($config->getValue('google_status')[0] == 'true' ) checked="checked" @endif>
+									<input type="radio" name="config[google_status]" class="flat-red" value="true" @if ($config->getConfigByKey('google_status') == 'true' ) checked="checked" @endif>
 									是
 								</label>&emsp;
 								<label>
-									<input type="radio" name="config[google_status]" class="flat-red" value="false" @if ($config->getValue('google_status')[0] == 'false' ) checked="checked" @endif>
+									<input type="radio" name="config[google_status]" class="flat-red" value="false" @if ($config->getConfigByKey('google_status') == 'false' ) checked="checked" @endif>
 									否
 								</label>&emsp;
 							</div>
@@ -180,28 +180,28 @@
 							<label for="config_google_json" class="col-sm-3 control-label">Google-Key</label>
 
 							<div class="col-sm-9">
-								<input type="text" class="form-control" id="config_google_json" name="config[google_json]" placeholder="Google auto login json" value="{{ $config->getValue('google_json')[0] }}">
+								<input type="text" class="form-control" id="config_google_json" name="config[google_json]" placeholder="Google auto login json" value="{{ $config->getConfigByKey('google_json') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_google_json" class="col-sm-3 control-label">Google認證用client_id</label>
 
 							<div class="col-sm-9">
-								<input type="text" class="form-control" id="config_google_client_id" name="config[google_google_client_id]" placeholder="Google client_id" value="{{ $config->getValue('google_client_id')[0] }}">
+								<input type="text" class="form-control" id="config_google_client_id" name="config[google_google_client_id]" placeholder="Google client_id" value="{{ $config->getConfigByKey('google_client_id') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_google_json" class="col-sm-3 control-label">Google認證用client_secret</label>
 
 							<div class="col-sm-9">
-								<input type="text" class="form-control" id="config_google_json" name="config[google_json]" placeholder="Google client_secret" value="{{ $config->getValue('google_client_secret')[0] }}">
+								<input type="text" class="form-control" id="config_google_json" name="config[google_json]" placeholder="Google client_secret" value="{{ $config->getConfigByKey('google_client_secret') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_google_json" class="col-sm-3 control-label">Google認證用回傳網址</label>
 
 							<div class="col-sm-9">
-								<input type="text" class="form-control" id="config_google_json" name="config[google_json]" placeholder="Google return url" value="{{ $config->getValue('google_redirect')[0] }}">
+								<input type="text" class="form-control" id="config_google_json" name="config[google_json]" placeholder="Google return url" value="{{ $config->getConfigByKey('google_redirect') }}">
 							</div>
 						</div>
 					</div>
@@ -225,11 +225,11 @@
 							<label for="form_config_slack_status" class="col-sm-2 control-label">啟用</label>
 							<div class="col-sm-10">
 								<label>
-									<input type="radio" name="config[slack_status]" class="flat-red" value="true" @if ($config->getValue('slack_status')[0] == 'true') checked="checked" @endif>
+									<input type="radio" name="config[slack_status]" class="flat-red" value="true" @if ($config->getConfigByKey('slack_status') == 'true') checked="checked" @endif>
 									是
 								</label>&emsp;
 								<label>
-									<input type="radio" name="config[slack_status]" class="flat-red" value="false" @if ($config->getValue('slack_status')[0]  == 'false') checked="checked" @endif>
+									<input type="radio" name="config[slack_status]" class="flat-red" value="false" @if ($config->getConfigByKey('slack_status')  == 'false') checked="checked" @endif>
 									否
 								</label>&emsp;
 							</div>
@@ -238,19 +238,19 @@
 							<label for="config_slack_token" class="col-sm-2 control-label">Token</label>
 
 							<div class="col-sm-5">
-								<input type="email" class="form-control" id="config_slack_token" name="config[slack_token]" placeholder="API Token" value="{{ $config->getValue('slack_token')[0] }}">
+								<input type="email" class="form-control" id="config_slack_token" name="config[slack_token]" placeholder="API Token" value="{{ $config->getConfigByKey('slack_token') }}">
 							</div>
 
 							<label for="config_slack_public_channel" class="col-sm-2 control-label">廣播頻道</label>
 							<div class="col-sm-3">
-								<input type="text" class="form-control" id="config_slack_public_channel" name="config[slack_public_channel]" placeholder="指定通知頻道" value="{{ $config->getValue('slack_public_channel')[0] }}">
+								<input type="text" class="form-control" id="config_slack_public_channel" name="config[slack_public_channel]" placeholder="指定通知頻道" value="{{ $config->getConfigByKey('slack_public_channel') }}">
 							</div>
 						</div>
 						<div class="form-group">
 							<label for="config_slack_botname" class="col-sm-2 control-label">BOT代號</label>
 
 							<div class="col-sm-10">
-								<input type="text" class="form-control" id="config_slack_botname" name="config[slack_botname]" placeholder="機器人代號" value="{{ $config->getValue('slack_botname')[0] }}">
+								<input type="text" class="form-control" id="config_slack_botname" name="config[slack_botname]" placeholder="機器人代號" value="{{ $config->getConfigByKey('slack_botname') }}">
 							</div>
 						</div>
 					</div>
