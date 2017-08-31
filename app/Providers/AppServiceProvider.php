6dcdfdc90ce5ddcfe3e27cc3c52d0afdf8887cd5
admin-PC\tony <tony@wabow.com>
2017-08-31 21:03:25 +0800

    特休計算方式重寫 測試結果正常

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,8 +16,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $start_date = "2018-01-01 00:00:00";
-        $enter_date = "2016-11-04 00:00:00";
+        $start_date = "2017-08-31 00:00:00";
+        $enter_date = "2016-11-07 00:00:00";
         $hours = "";
         $annual_date = 0;
 
@@ -25,31 +25,52 @@ class AppServiceProvider extends ServiceProvider
         $enter_date_year = date('Y', strtotime($enter_date));
         $service_year = intval($start_date_year - $enter_date_year );
 
+        $start_date_month = ($start_date=="") ? date('m', strtotime(Carbon::now())) : date('m', strtotime($start_date));
+        $enter_date_month = date('m', strtotime($enter_date));
+        $service_month = intval($start_date_month - $enter_date_month);
+
+        $start_date_day = ($start_date=="") ? date('d', strtotime(Carbon::now())) : date('d', strtotime($start_date));
+        $enter_date_day = date('d', strtotime($enter_date));
+        $service_day = intval($start_date_day - $enter_date_day);
+
+        if (($start_date_month - $enter_date_month) <0 ) {
+
+            $service_year --;
+
+        }
+
         switch ($service_year) {
             case 0:
-            case 1:
-                $enter_date_month = intval(date('m', strtotime($enter_date)));
-                if ($enter_date_month <=6) {
+                if ($start_date_year != $enter_date_year) {
 
-                    $annual_date = 3;
+                    $service_month += 12;
+                    if ($service_month > 6) {
+
+                        $annual_date = 3;
+
+                    } elseif($service_month == 6 && $service_day >= 0) {
+
+                        $annual_date = 3;
+
+                    }
 
                 }
                 break;
-            case 2:
+            case 1:
                 $annual_date = 7;
                 break;
-            case 3:
+            case 2:
                 $annual_date = 10;
                 break;
+            case 3:
             case 4:
-            case 5:
                 $annual_date = 14;
                 break;
-            case ($service_year > 5 && $service_year <= 10):
+            case ($service_year >= 5 && $service_year < 10):
                 $annual_date = 15;
                 break;
-            case ($service_year > 10):
-                $annual_date = ($service_year + 4 < 30) ? $service_year + 4 : 30;
+            case ($service_year >= 10):
+                $annual_date = ($service_year + 6 < 30) ? $service_year + 6 : 30;
                 break;
         }
 
