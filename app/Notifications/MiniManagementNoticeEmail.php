6ad5b691c39d2db903c67f2f21fc66c2ca9541e3
eno <eno@wabow.com>
2017-09-20 20:57:29 +0800

    更新部分Slack、Email通知文本

--- app/Notifications/MiniManagementNoticeEmail.php
+++ app/Notifications/MiniManagementNoticeEmail.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 
-class AgentNoticeEmail extends Notification
+class MiniManagementNoticeEmail extends Notification
 {
     use Queueable;
 
@@ -49,9 +49,9 @@ class AgentNoticeEmail extends Notification
         return (new MailMessage)
             ->from(Config::getConfigValueByKey("smtp_from") , Config::getConfigValueByKey("smtp_display"))
             ->subject("假單申請通知". Config::getConfigValueByKey("smtp_display"))
-            ->line($this->name.' 於 '.$this->start_time." 將請假並指定您為代理人")
+            ->line($this->name.' 於 '.$this->start_time." 將請假")
             ->line('請盡速進行確認是否同意，謝謝。')
-            ->action('點我進入', route("agent/index"));
+            ->action('點我進入', route("leaves/manager/prove")."/Mini_Manager");
     }
 
 }
