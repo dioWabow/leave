a3e6e1ba6dc6e94648554106cb4c0f6626d08fda
admin-PC\tony <tony@wabow.com>
2017-10-20 09:47:19 +0800

    修正畫面

--- resources/views/sheet_search.blade.php
+++ resources/views/sheet_search.blade.php
@@ -33,6 +33,7 @@
                   <label>
                     每頁 
                     <select name="order_by[pagesize]" class="form-control input-sm" onchange="javascript:changePageSize(this.value);">
+                      <option value="25"selected="selected">25</option>
                       @if(1!=1)
                       <option value="25"@if($model->pagesize=="25")selected="selected"@endif>25</option>
                       <option value="50"@if($model->pagesize=="50")selected="selected"@endif>50</option>
@@ -45,7 +46,7 @@
                 <div class="col-sm-9">
                     <div class="pull-right">
                       <label>
-                        專案名稱：
+                        專案：
                         <select id="search_teams" name="search[teams]" class="form-control">
                           <option value="" selected="selected">全部</option>
                           @if(1!=1)
@@ -67,7 +68,7 @@
                       &nbsp;
                       <label>
                         關鍵字：
-                        <input type="search" class="form-control" placeholder="" style="width:270px" value="">
+                        <input type="search" class="form-control" placeholder="請輸入填寫人、標籤、標題、內容" style="width:270px" value="">
                         @if(1!=1)
                         <input type="search" class="form-control" placeholder="請輸入員編、英文名、中文名進行查詢" name="search[keywords]" style="width:270px" value="@if(count($search)>0){{$search['keywords']}}@endif">
                         @endif
@@ -84,7 +85,7 @@
                       <tr>
                         <th width="3%"></th>
                         <th width="5%"><a href="javascript:void(0)" onclick="changeSort('employee_no');">填寫人</a></th>
-                        <th><a href="javascript:void(0)" onclick="changeSort('nickname');">專案名稱</a></th>
+                        <th><a href="javascript:void(0)" onclick="changeSort('nickname');">專案</a></th>
                         <th><a href="javascript:void(0)" onclick="changeSort('name');">標題</a></th>
                         <th width="8%"><a href="javascript:void(0)" onclick="changeSort('birthday');">內容</a></th>
                         <th width="8%"><a href="javascript:void(0)" onclick="changeSort('enter_date');">時數</a></th>
@@ -93,61 +94,29 @@
                       </tr>
                     </thead>
                     <tbody>
-                      @if(1!=1)
-                      @forelse($dataProvider as $user)
                       <tr class='clickable-row' data-href="{{ route('user/edit', ['id'=>$user->id]) }}">
                         <td align="center"><img src="{{UrlHelper::getUserAvatarUrl($user->avatar)}}" class="img-circle" alt="{{$user->nickname}}" width="50px"></td>
                         <td align="center">
-                          <div>{{$user->employee_no}}</div>
-                          <div>
-                            <small class="label bg-red">
-                              {{UserHelper::getRoleByUserId($user->id)}}
-                            </small>
-                          </div>
                         </td>
-                        <td>{{$user->nickname}}</td>
-                        <td>{{$user->name}}</td>
-                        <td>{{$user->birthday}}</td>
-                        <td>{{date('Y-m-d', strtotime($user->enter_date))}}</td>
-                        <td>
-                          @if ($user->leave_date == "")
-                            -
-                          @else
-                            {{date('Y-m-d', strtotime($user->leave_date))}}
-                          @endif
-                        </td>
-                        <td>
-                          @if ($user->arrive_time == "0900")
-                            09:00 - 18:00
-                          @else
-                            09:30 - 18:30
-                          @endif
-                        </td>
-                        <td>
-                          @foreach (App\UserAgent::getUserAgentByUserId($user->id) as $agent)
-                            @if($agent->fetchUser->status == 1)
-                            <img src="{{UrlHelper::getUserAvatarUrl($agent->fetchUser->avatar)}}" class="img-circle" alt="{{$agent->fetchUser->avatar}}" width="50px">
-                            @endif
-                          @endforeach
-                        </td>
-                        <td>
-                          @foreach (App\UserTeam::getUserTeamByUserId($user->id) as $user_team)
-                            @if(!empty($user_team->fetchTeam))
-                              <small class="label" style="background-color:{{$user_team->fetchTeam->color}};">{{$user_team->fetchTeam->name}}</small>
-                            @endif
-                          @endforeach
-                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td align="center"></td>
+                      </tr>
+                      @if(1!=1)
+                      @forelse($test as $test1)
+                      <tr class='clickable-row' data-href="{{ route('user/edit', ['id'=>$user->id]) }}">
+                        <td align="center"><img src="{{UrlHelper::getUserAvatarUrl($user->avatar)}}" class="img-circle" alt="{{$user->nickname}}" width="50px"></td>
                         <td align="center">
-                          @if ($user->status == "1")
-                            <small class="badge bg-green" title="在職中">on</small>
-                          @else
-                            <small class="badge bg-dark" title="已離職">OFF</small>
-                          @endif
                         </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td align="center"></td>
                       </tr>
                       @empty
                         <tr>
-                          <td colspan="11" align="center">無資料</td>
+                          <td colspan="8" align="center"><span class="glyphicon glyphicon-search"> 沒有查詢到相關結果</span></td>
                         </tr>
                       @endforelse
                       @endif
